ebbd6b1ee019c76fa3524eeaa2456e0975781876
Gaspar <gaspar.feron@groups.be>
2016-10-05 09:48:47 +0200

    Inject SVG before mounting

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import m from 'mithril/hyperscript';
 import mount from 'mithril/mount';
-import inject from 'svg-injector';
+import injectSvg from 'svg-injector';
 
 function oninit(vnode) {
 	vnode.state.flag = true;
@@ -8,13 +8,15 @@ function oninit(vnode) {
 
 function view(vnode) {
 	return [
-		m('img[src="sprite.svg"]', {oncreate: vnode => inject(vnode.dom)}),
 		m('ul', {onclick: () => vnode.state.flag = !vnode.state.flag}, [
-			m('li', {style: 'background: yellow; width: 33%;'}, vnode.state.flag),
+			m('li', {style: 'background: green; width: 33%;'}, vnode.state.flag),
 			m('li', {style: 'background: yellow; width: 33%;'}, vnode.state.flag ? m('label', m('svg', m('use[href="#ic_delete_24px"]'))) : null),
 			m('li', {style: 'background: blue; width: 33%;'}, vnode.state.flag ? m('label', m('svg', m('use[href="#ic_edit_24px"]'))) : m('svg', m('use[href="#ic_check_24px"]')))
 		])
 	];
 }
+
+const sprite = document.querySelector('#sprite');
+injectSvg(sprite);
 
 mount(document.getElementById('app'), {oninit, view});
--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 function oninit(vnode) {
-	vnode.flag = true;
+	vnode.state.flag = true;
 }
 
 function view(vnode) {
 	return [
-		m('img[src="sprite.svg"]', {oncreate: function(vnode) {SVGInjector(vnode.dom)}}),
 		m('ul', {onclick: function() {vnode.state.flag = !vnode.state.flag}}, [
-			m('li', {style: 'background: yellow; width: 33%;'}, vnode.state.flag),
-			m('li', vnode.state.flag ? m('label', m('svg', m('use[href="#ic_delete_24px"]'))) : null),
+			m('li', {style: 'background: green; width: 33%;'}, vnode.state.flag),
+			m('li', {style: 'background: yellow; width: 33%;'}, vnode.state.flag ? m('label', m('svg', m('use[href="#ic_delete_24px"]'))) : null),
 			m('li', {style: 'background: blue; width: 33%;'}, vnode.state.flag ? m('label', m('svg', m('use[href="#ic_edit_24px"]'))) : m('svg', m('use[href="#ic_check_24px"]')))
 		])
 	];
 }
+
+const sprite = document.querySelector('#sprite');
+SVGInjector(sprite);
 
 m.mount(document.getElementById('app'), {oninit: oninit, view: view});
